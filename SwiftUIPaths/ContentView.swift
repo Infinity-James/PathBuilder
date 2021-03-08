681a1693501fa9ab9475c0b54c09fbf22cc1e6c2
James Valaitis <jamesvalaitis@gmail.com>
2021-03-08 10:54:25 +0000

    Initial Commit

---
+++ SwiftUIPaths/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIPaths
+//
+//  Created by James Valaitis on 08/03/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIPaths/SwiftUIPathsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPathsApp.swift
+//  SwiftUIPaths
+//
+//  Created by James Valaitis on 08/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPathsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
